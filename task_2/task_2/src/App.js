cedd6a96613d797c105df52824f7263c015aff35
Marianna Mori <marianna.mori1@gmail.com>
2020-10-26 13:22:35 +0200

    first part of the task

---
+++ task_2/task_2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App(props) {
+  
+    return (
+      <div className="container">
+      <h1>Hello World!</h1>
+      </div>
+    );
+  }
+
+
+export default App;
