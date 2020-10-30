7965d439ad9fae3542095906f6c20ccff7a029a5
Marianna Mori <marianna.mori1@gmail.com>
2020-10-30 10:31:33 +0200

    completed up to 6

---
+++ task_3/task_3/src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+import "./App.css";
+
+
+
+class App extends Component {
+  state = {
+    likes: 0,
+  };
+
+addHandler = () =>{
+
+  this.setState({ likes: this.state.likes +1 });
+};
+
+removeHandler = () =>{
+
+  this.setState({ likes: this.state.likes -1 });
+};
+
+resetHand = () =>{
+ 
+  this.setState({ likes: 0 });
+};
+
+  render() {
+    return (
+      <div>
+        <h1>Total likes: {this.state.likes}</h1>
+        <button onClick={this.addHandler}>Add one</button>
+        <button onClick={this.removeHandler}>Remove one</button>
+        <button onClick={this.resetHand}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default App;
