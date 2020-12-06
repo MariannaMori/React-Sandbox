fed53a70b2b32487ba804ba31e518038f27f1b7f
Marianna Mori <marianna.mori1@gmail.com>
2020-12-06 15:10:06 +0200

    margits version as a base

---
+++ 6_task/src/Form/Form.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Form.css";
+
+const Form = ({ submit, change, role }) => {
+  return (
+    <div className="form">
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="firstname">First name</label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            onChange={change}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastname">Last name</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            onChange={change}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phonenumber">Phone number</label>
+          <input
+            type="phone"
+            name="phonenumber"
+            id="phonenumber"
+            onChange={change}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            onChange={change}
+            required
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="role">Role</label>
+          <select name="role" value={role} onChange={change}>
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
