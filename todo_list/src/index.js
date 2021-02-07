7b3e29bef263a4798ee90e4842ae49a8485cb46e
Marianna Mori <marianna.mori1@gmail.com>
2021-02-07 15:08:31 +0200

    added todo components

--- todo_list/src/index.js
+++ todo_list/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+import reactDom from 'react-dom';
+
+reactDom.render ((
+    <Provider store={store}>
+        <App />
+    </Provider>
+), document.getElementById('root'));
+registerServiceWorker();
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
 
 
 
